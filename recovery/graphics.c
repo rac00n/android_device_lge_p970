c17585a0d5d560423a9db1dd62c419a13d8b1229
rac00n <w4shb33r@gmail.com>
2013-11-07 01:31:27 +0100

    Disable gr_text function
    Change-Id: I6d3c39781e765b3bbfc30992fc41d03c01d06076

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -179,7 +179,7 @@ int gr_measure(const char *s)
     return gr_font->cwidth * strlen(s);
 }
 
-int gr_text(int x, int y, const char *s)
+/*int gr_text(int x, int y, const char *s)
 {
     GGLContext *gl = gr_context;
     GRFont *font = gr_font;
@@ -203,7 +203,7 @@ int gr_text(int x, int y, const char *s)
     }
 
     return x;
-}
+}*/
 
 void gr_fill(int x, int y, int w, int h)
 {
