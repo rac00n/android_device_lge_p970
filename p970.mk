ca63e2d029fa04ddac7cf6b8f5d53ca54534fa93
rac00n <w4shb33r@gmail.com>
2014-01-26 22:38:52 +0100

    add wifi overlay configs
    Change-Id: I6c1854ce647b9b90ef1f2debd8f4989b43ebb167

--- p970.mk
+++ p970.mk
@@ -59,6 +59,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifimac/wlan-precheck:system/bin/wlan-precheck \
     $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/nvram.txt:system/etc/wifi/nvram.txt \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
 
 ## Touchscreen confs
